534e445bf5fe021314d4121ef2c351367a60787f
Chaitanya <chaitanyanarang12@gmail.com>
2025-03-06 12:33:55 +0530

    SELECTION , BUBBLE AND INSERTION SORT DONE ON 06-03-2025

--- SORTING/INSERTION.cpp
+++ SORTING/INSERTION.cpp
@@ -1,34 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void insertion_sort(int n, int arr[]) {
-    for (int i = 1; i < n; i++) { // Start from the 2nd element (index 1)
-        int key = arr[i]; // Current element to be placed correctly
-        int j = i - 1;
-
-        // Shift elements of arr[0..i-1] that are greater than key
-        while (j >= 0 && arr[j] > key) {
-            arr[j + 1] = arr[j];
+void insertion_sort(int n, int arr[]) 
+{
+    for(int i=0;i<n;i++)
+    {
+        int key=i;
+        int j=i-1;
+        while(j>=0 && arr[j]>key)
+        {
+            arr[j+1]=arr[j];
             j--;
         }
-        
-        arr[j + 1] = key; // Insert key at the correct position
+        arr[j+1]=key;
     }
 }
 
-int main() {
+int main() 
+{
     int n;
     cout << "Enter the number of elements in the array: ";
     cin >> n;
     int arr[n];
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) 
+    {
         cout << "Enter element " << i + 1 << ": ";
         cin >> arr[i];
     }
 
     cout << "Original array: ";
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) 
+    {
         cout << arr[i] << " ";
     }
     cout << endl;
@@ -36,7 +39,8 @@ int main() {
     insertion_sort(n, arr);
 
     cout << "Sorted array: ";
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) 
+    {
         cout << arr[i] << " ";
     }
     cout << endl;
