df46ce9c1dd58a49d3282eb46b54655d65824e66
Chaitanya <chaitanyanarang12@gmail.com>
2025-07-17 13:58:06 +0530

    STL LEARNED ON 17-07-2025

--- STL/BASICS.cpp
+++ STL/BASICS.cpp
@@ -1,6 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+class person{
+    //CLASS FOR SET
+    public:
+    int age;
+    string name;
+
+    bool operator < (const person &other) const{
+        return age<other.age;
+        // return age<other.age;   TO PRINT IN REVERSE ORDER
+    }
+};
+
 int main()
 {
 //PAIR
@@ -58,5 +70,114 @@ int main()
     cout<<endl<<endl;
 
 //SET
+    cout<<"SETS"<<endl;
+    set<int>s1; //SET IN ASCENDING ORDER
+    s1.insert(10);   //INSERT FUNCTION
+    s1.insert(20);
+    s1.insert(110);
+    s1.insert(10);
+    s1.insert(30);
+    s1.insert(200);
+    s1.insert(20);
+    s1.insert(210);
+    cout<<"Set In Ascending Order: ";
+    for(auto it=s1.begin(); it!=s1.end();it++)
+    {
+        cout<<*it<<" "; //PRINTING ALL VALUES
+    }
+    cout<<endl;
+
+    set<int, greater<int>>s2;  //SET IN DESCENDING ORDER
+    s2.insert(10);   //INSERT FUNCTION
+    s2.insert(20);
+    s2.insert(110);
+    s2.insert(10);
+    s2.insert(30);
+    s2.insert(200);
+    s2.insert(20);
+    s2.insert(210);
+    cout<<"Set In Descending Order: ";
+    for(auto it=s2.begin(); it!=s2.end();it++)
+    {
+        cout<<*it<<" "; //PRINTING ALL VALUES
+    }
+    cout<<endl;
+
+    if(s1.find(200)!=s1.end()) cout<<"Present";  //CHECKING IF ELEMENT IS PRESENT
+    else cout<<"Not Present";
+    cout<<endl;
+
+    if(s1.count(25)) cout<<"Present"; //METHOD 2 FOR CHECKING IF ELEMENT IS PRESENT
+    else cout<<"Not Present"; 
+    cout<<endl;
+
+    s1.erase(110);  //DELETING ELEMENT
+    cout<<"Set After deletion: ";
+    for(auto it=s1.begin(); it!=s1.end();it++)
+    {
+        cout<<*it<<" "; 
+    }
+    cout<<endl;
+
+    set<person>s3;
+    person a1,a2,a3;
+    a1.age=10, a1.name="Chaitanya";
+    a2.age=20, a2.name="XYZ";
+    a3.age=5, a3.name="ABC";
+    
+    s3.insert(a1);
+    s3.insert(a2);
+    s3.insert(a3);
+    cout<<"SET MADE OF CLASS: ";
+    for(auto it=s3.begin();it!=s3.end();it++)
+    {
+        cout<<(*it).age<<" "<<(*it).name<<endl;
+    }
+    cout<<endl;
+
+    multiset<int> s4;  //MULTISET ALLOWS DUPLICATE VALUES
+    s4.insert(10);
+    s4.insert(20);
+    s4.insert(10);
+    s4.insert(40);
+    cout<<"MULTI-SET: ";
+    for(auto it=s4.begin(); it!=s4.end();it++)
+    {
+        cout<<*it<<" "; 
+    }
+    cout<<endl<<endl;
+
+//UNORDERED SET
+    cout<<"UNORDERED SET: ";
+    unordered_set<int>u1;
+    u1.insert(10);   //INSERT FUNCTION
+    u1.insert(20);
+    u1.insert(110);
+    u1.insert(10);
+    u1.insert(30);
+    u1.insert(200);
+    u1.insert(20);
+    u1.insert(210);
+    for(auto it=u1.begin(); it!=u1.end();it++)
+    {
+        cout<<*it<<" "; 
+    }
+    cout<<endl;
+
+    cout<<"UNORDERED MULTI SET: ";
+    unordered_multiset<int>u2;
+    u2.insert(10);   //INSERT FUNCTION
+    u2.insert(20);
+    u2.insert(110);
+    u2.insert(10);
+    u2.insert(30);
+    u2.insert(200);
+    u2.insert(20);
+    u2.insert(210);
+    for(auto it=u2.begin(); it!=u2.end();it++)
+    {
+        cout<<*it<<" "; 
+    }
+    cout<<endl;
 
 }
